d89ce700483d8f109d86d0419050fb57bd7b49d3
Phoenix <toni_11.tebar@hotmail.com>
2025-05-02 18:26:30 +0200

    curso

--- 04.js
+++ 04.js
@@ -1,8 +1,27 @@
-let a = 5;
+const numeros = [1,2,3,4,5,6,7,8,9]
 
-let b = 3; 
 
-console.log(a & b)
-console.log(a | b)
-console.log(a ^ b)
-console.log(~a)
+console.log(numeros)
+
+
+console.log(numeros[numeros.indexOf(8)])
+
+const meses = ['Enero', 'Febrero', 'Marzo','Abril']
+
+console.table(meses)
+
+for(let i=0;i<meses.length; i++){
+    console.log(meses[i])
+}
+
+meses.push('Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+
+console.table(meses)
+
+for(let i=0;i<meses.length; i++){
+    console.log(meses[i])
+}
+
+numeros.splice(2,1)
+
+console.log(numeros)
